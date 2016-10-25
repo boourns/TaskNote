e3f65a890f8d7d2a68ff1eaa0287bb6c2857def2
Tom Burns <tom.burns@shopify.com>
2016-10-24 21:54:55 -0400

    first version of TaskNote

---
+++ TaskNote/AppDelegate.swift
@@ -0,0 +1,84 @@
+//
+//  AppDelegate.swift
+//  TaskNote
+//
+//  Created by Tom Burns on 2016-10-24.
+//  Copyright © 2016 BurnsMod. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+	var statusItem: NSStatusItem!
+
+	let key = "TaskNoteValue"
+	let defaultNote = "TaskNote is empty"
+
+	var note: String {
+		get {
+			return UserDefaults.standard.string(forKey: key) ?? defaultNote
+		}
+
+		set {
+			UserDefaults.standard.set(newValue, forKey: key)
+			updateTitle()
+		}
+	}
+
+	private func updateTitle() {
+		statusItem.title = note
+	}
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+		self.statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+		statusItem.isEnabled = true
+		updateTitle()
+		createMenu()
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+	func editNotePressed(sender: NSMenuItem) {
+		let alert = NSAlert()
+		let input = NSTextField(frame: NSRect(x: 0, y: 0, width: 300, height: 24))
+		alert.messageText = "Enter new taskbar note"
+		input.stringValue = note
+		alert.accessoryView = input
+
+		alert.addButton(withTitle: "Ok")
+		alert.addButton(withTitle: "Cancel")
+		switch(alert.runModal()) {
+		case NSAlertFirstButtonReturn:
+			note = input.stringValue
+		default:
+			break
+		}
+	}
+
+	@objc func quit(sender: NSMenuItem) {
+		exit(0)
+	}
+
+	private func createMenu() {
+		let menu = NSMenu()
+
+		let editNoteItem = NSMenuItem(title: "Edit Note", action: #selector(editNotePressed(sender:)), keyEquivalent: "e")
+		editNoteItem.target = self
+		menu.addItem(editNoteItem)
+
+		menu.addItem(NSMenuItem.separator())
+
+		let quitItem = NSMenuItem(title: "Quit TaskNote", action: #selector(quit(sender:)), keyEquivalent: "q")
+		quitItem.target = self
+		menu.addItem(quitItem)
+
+		statusItem.menu = menu
+	}
+}
+
